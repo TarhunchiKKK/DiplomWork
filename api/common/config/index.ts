fd1c0e4337b27b8552d8390b24115045c3cfea7d
“TarhunchicKKK” <kostabarilo12@gmail.com>
2025-04-13 13:48:13 +0300

    feat(api): reCAPTCHA

--- api/common/config/index.ts
+++ api/common/config/index.ts
@@ -2,3 +2,4 @@ export { getGrpcConfig } from "./grpc.config";
 export { getJwtConfig } from "./jwt.config";
 export { getMailerConfig } from "./mailer.config";
 export { getRabbitMqConfig } from "./rabbitmq.config";
+export { getRecaptchaConfig } from "./recaptcha.config";
