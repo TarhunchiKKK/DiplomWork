dafcae0f53f4714a7657731b7588c3ab548f9f5e
“TarhunchicKKK” <kostabarilo12@gmail.com>
2025-04-22 18:16:10 +0300

    feat(api): database connection in notifications repository

--- api/apps/notifications/src/app.module.ts
+++ api/apps/notifications/src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { NotificationsModule } from "./notifications/notifications.module";
 import { UserNotificationsModule } from "./user/user-notifications.module";
 import { DocumentNotificationsModule } from "./document/document-notifications.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Notification } from "./notifications/entities/notification.entity";
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true
         }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: "postgres",
+                database: configService.getOrThrow<string>("NOTIFICATIONS_MICROSERVICE_DB_NAME"),
+                host: configService.getOrThrow<string>("NOTIFICATIONS_MICROSERVICE_DB_HOST"),
+                port: +configService.getOrThrow<number>("NOTIFICATIONS_MICROSERVICE_DB_PORT"),
+                username: configService.getOrThrow<string>("NOTIFICATIONS_MICROSERVICE_DB_USER"),
+                password: configService.getOrThrow<string>("NOTIFICATIONS_MICROSERVICE_DB_PASSWORD"),
+                synchronize: true,
+                entities: [Notification]
+            })
+        }),
+
         NotificationsModule,
         UserNotificationsModule,
         DocumentNotificationsModule
--- api/apps/notifications/src/notifications/notifications.service.ts
+++ api/apps/notifications/src/notifications/notifications.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Notification } from "./entities/notification.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+    public constructor(
+        @InjectRepository(Notification)
+        private readonly notificcationsRepository: Repository<Notification>
+    ) {}
+}
