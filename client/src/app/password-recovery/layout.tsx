3874eb31f9dbc77725074b122a23b929796da34e
TarhunchiKKK <kostabarilo12@gmail.com>
2025-06-06 15:54:14 +0300

    fix(client): fix RequireAuth using

--- client/src/app/password-recovery/layout.tsx
+++ client/src/app/password-recovery/layout.tsx
@@ -4,14 +4,14 @@ import { PropsWithChildren } from "react";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
     return (
-        <main>
-            <div className="p-4">
-                <ThemeSwitch />
-            </div>
+        <RequireAuth>
+            <main>
+                <div className="p-4">
+                    <ThemeSwitch />
+                </div>
 
-            <RequireAuth>
                 <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{children}</div>
-            </RequireAuth>
-        </main>
+            </main>
+        </RequireAuth>
     );
 }
