b673bede5297742027de9329171f14baeeff56f6
TarhunchiKKK <kostabarilo12@gmail.com>
2025-06-08 13:55:38 +0300

    feat(api): connect to SMTP

--- api/common/modules/mails/mails.service.ts
+++ api/common/modules/mails/mails.service.ts
@@ -1,17 +1,25 @@
-import { MailerService } from "@nestjs-modules/mailer";
 import { Injectable } from "@nestjs/common";
 import { ISendMailDto } from "./dto/send-mail.dto";
+import { Resend } from "resend";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class MailsService {
-    public constructor(readonly mailerService: MailerService) {}
+    private readonly resend: Resend;
+
+    private readonly from: string;
+
+    public constructor(private readonly configService: ConfigService) {
+        this.resend = new Resend(this.configService.getOrThrow<string>("MAILS_KEY"));
+
+        this.from = this.configService.getOrThrow<string>("MAILS_FROM");
+    }
 
     public sendMail(dto: ISendMailDto) {
         try {
-            this.mailerService.sendMail({
-                to: dto.to,
-                subject: dto.subject,
-                html: dto.html
+            this.resend.emails.send({
+                ...dto,
+                from: this.from
             });
         } catch (error: unknown) {
             console.log("SMTP error:");
